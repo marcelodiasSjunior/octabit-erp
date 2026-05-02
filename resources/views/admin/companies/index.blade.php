8f6860a4f22e1b6e9bedf18b746c6e4558137a1a
unknown <mdsj@outlook.com>
2026-05-02 14:24:25 -0300

    fix: update buttons to link to creation forms

--- resources/views/admin/companies/index.blade.php
+++ resources/views/admin/companies/index.blade.php
@@ -23,7 +23,7 @@
             <button type="submit" class="btn-secondary">Filtrar</button>
         </form>
 
-        <a id="btn-create-company" href="#" class="btn-primary whitespace-nowrap self-start sm:self-auto">
+        <a id="btn-create-company" href="{{ route('admin.companies.create') }}" class="btn-primary whitespace-nowrap self-start sm:self-auto">
             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
             </svg>
--- resources/views/settings/users/index.blade.php
+++ resources/views/settings/users/index.blade.php
@@ -7,18 +7,12 @@
             <p class="text-xs text-slate-500">Gerencie os membros da sua equipe e seus níveis de acesso.</p>
         </div>
 
-        <button id="btn-invite-user" class="btn-primary whitespace-nowrap self-start sm:self-auto"
-                x-data @click="$dispatch('dialog', { 
-                    title: 'Convidar Novo Usuário', 
-                    message: 'Esta funcionalidade permitirá enviar um convite por e-mail para um novo membro da equipe.',
-                    type: 'confirm',
-                    confirmText: 'Entendi'
-                })">
+        <a id="btn-create-user" href="{{ route('settings.users.create') }}" class="btn-primary whitespace-nowrap self-start sm:self-auto">
             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"/>
             </svg>
             Novo Usuário
-        </button>
+        </a>
     </div>
 
     {{-- Users Table --}}
